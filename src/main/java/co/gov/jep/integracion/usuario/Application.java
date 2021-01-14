2e71e1a13ca0bc20ca3f19387e846bb80676d96b
DESKTOP-O2IJLB3\Kubit <yosmelinmarquez@gmail.com>
2021-01-14 12:02:40 -0500

    cambiarContrasenia

--- src/main/java/co/gov/jep/integracion/usuario/Application.java
+++ src/main/java/co/gov/jep/integracion/usuario/Application.java
@@ -13,7 +13,7 @@
  *  implied.  See the License for the specific language governing
  *  permissions and limitations under the License.
  */
-package co.gov.jep.integracion.contacto;
+package co.gov.jep.integracion.usuario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
