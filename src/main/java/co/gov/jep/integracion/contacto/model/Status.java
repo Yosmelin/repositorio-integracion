f133e8e6f05a120aae9dce01653e87bb3f45ac98
DESKTOP-O2IJLB3\Kubit <yosmelinmarquez@gmail.com>
2021-01-15 15:12:23 -0500

    actualizar contacto

---
+++ src/main/java/co/gov/jep/integracion/contacto/model/Status.java
@@ -0,0 +1,28 @@
+package co.gov.jep.integracion.contacto.model;
+
+public class Status {
+	
+	private String StatusCode;
+	
+	private String StatusDesc;
+
+	public String getStatusCode() {
+		return StatusCode;
+	}
+
+	public void setStatusCode(String statusCode) {
+		StatusCode = statusCode;
+	}
+
+	public String getStatusDesc() {
+		return StatusDesc;
+	}
+
+	public void setStatusDesc(String statusDesc) {
+		StatusDesc = statusDesc;
+	}
+	
+	
+	
+
+}
